54261e41bc857f497428d7cd05923d61e816bb71
ganesh-48 <ganesh.rananavare98@gmail.com>
2021-03-29 09:22:05 +0530

    ADDED: Add new contacts intp address book

--- src/com/addressbook/AddressBookProblem.java
+++ src/com/addressbook/AddressBookProblem.java
@@ -1,10 +1,49 @@
 package com.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBookProblem {
+
+	Contacts contacts;
+
+	// Add new contact to address book
+	public void addContacts() {
+		String firstName, lastName, address, city, state, email;
+		int zip;
+		long number;
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the first name :");
+		firstName = sc.nextLine();
+		System.out.println("Enter the last name :");
+		lastName = sc.nextLine();
+		System.out.println("Enter the address :");
+		address = sc.nextLine();
+		System.out.println("Enter the city :");
+		city = sc.nextLine();
+		System.out.println("Enter the state :");
+		state = sc.nextLine();
+		System.out.println("Enter the email :");
+		email = sc.nextLine();
+		System.out.println("Enter the zip :");
+		zip = sc.nextInt();
+		System.out.println("Enter the number :");
+		number = sc.nextLong();
+
+		contacts = new Contacts(firstName, lastName, address, city, state, zip, number, email);
+		System.out.println("Contact added");
+	}
+
+	// print contact details
+	public String toString() {
+		return contacts.toString();
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to address book System");
 
-		Contacts contact = new Contacts("Ram", "Sharma", "Line - 3", "maharashtra", "solapur", 123456, 1234567899, "abc@gmail.com");
-		System.out.println(contact.toString());
+		AddressBookProblem addressbook = new AddressBookProblem();
+		addressbook.addContacts();
+		System.out.println(addressbook.toString());
 	}
 }
