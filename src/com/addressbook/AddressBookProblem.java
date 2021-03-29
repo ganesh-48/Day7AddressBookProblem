3bff473650d8414b70eaea30c0fd556e58763488
ganesh-48 <ganesh.rananavare98@gmail.com>
2021-03-29 08:46:29 +0530

    ADDED: Added Created contacts in address book

--- src/com/addressbook/AddressBookProblem.java
+++ src/com/addressbook/AddressBookProblem.java
@@ -3,5 +3,8 @@ package com.addressbook;
 public class AddressBookProblem {
 	public static void main(String[] args) {
 		System.out.println("Welcome to address book System");
+
+		Contacts contact = new Contacts("Ram", "Sharma", "Line - 3", "maharashtra", "solapur", 123456, 1234567899, "abc@gmail.com");
+		System.out.println(contact.toString());
 	}
 }
