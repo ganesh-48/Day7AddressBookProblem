3b3b7373099fb05abf694fe7131e24711ff4d870
ganesh-48 <ganesh.rananavare98@gmail.com>
2021-03-29 10:13:04 +0530

    ADDED: Edit contact details of persons

--- src/com/addressbook/AddressBookProblem.java
+++ src/com/addressbook/AddressBookProblem.java
@@ -33,7 +33,39 @@ public class AddressBookProblem {
 		contacts = new Contacts(firstName, lastName, address, city, state, zip, number, email);
 		System.out.println("Contact added");
 	}
+	
+//edit contacts details
+	public void editContact(String firstName) {
+		if (contacts == null)
+			System.out.println("Address book is empty");
+		else {
+			if (contacts.getFirstName().equalsIgnoreCase(firstName)) {
+				System.out.println("edit the contact details");
+				Scanner sc = new Scanner(System.in);
+				System.out.println("enter the first name");
+				contacts.setFirstName(sc.next());
+				System.out.println("enter the last name");
+				contacts.setLastName(sc.next());
+				System.out.println("enter address");
+				contacts.setAddress(sc.next());
+				System.out.println("enter the state");
+				contacts.setState(sc.next());
+				System.out.println("enter the city");
+				contacts.setCity(sc.next());
+				System.out.println("enter the zip");
+				contacts.setZip(sc.nextInt());
+				System.out.println("enter mobile number");
+				contacts.setNumber(sc.nextLong());
+				System.out.println("enter the email");
+				contacts.setEmail(sc.next());
 
+				System.out.println("Contact is updated");
+			} else
+				System.out.println("contact not found");
+		}
+	}
+	
+	
 	// print contact details
 	public String toString() {
 		return contacts.toString();
@@ -43,7 +75,12 @@ public class AddressBookProblem {
 		System.out.println("Welcome to address book System");
 
 		AddressBookProblem addressbook = new AddressBookProblem();
+		Scanner sc = new Scanner(System.in);
+
 		addressbook.addContacts();
+		System.out.println("Enter the first name of contact :");
+		addressbook.editContact(sc.next());
 		System.out.println(addressbook.toString());
+	
 	}
 }
